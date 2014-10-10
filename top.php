d991c55923d057c6869c4d3cc417ac54026b7a5e
caro04152 <caro04152@users.noreply.github.com>
2014-10-10 13:02:09 -0400

    Create top.php

---
+++ top.php
@@ -0,0 +1,9 @@
+ <?php
+	require_once "1.php";
+ ?>
+<html>
+	<meta http-equiv="Content-Type" content="text/html; charset=gb2312">
+	<head>
+		<title>my work</title>
+		<link href="css/style.css" rel="stylesheet">
+	</head>
